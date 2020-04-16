7a28ff4cefcaaa6a961cc1c71129822f2b31da08
SiZhuXinxin <chengruixin2018@gmail.com>
2020-04-16 19:42:35 +0930

    fasdf

--- jsx_demo/src/index.js
+++ jsx_demo/src/index.js
@@ -12,8 +12,22 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 // function-based component
-const App = function() {
-    return <div>Hellasdfo!!!</div>;
+const App = () => {
+    const buttonText = 'Clickasdfas me!';
+    const getButtonText = () => {
+        return "helo changed!";
+    }
+    //return <div>Hellasdfo!!!</div>;
+    return (
+        <div style={{border:'1px solid black', width:'50%'}}>
+            <label className="dontKnow" htmlFor="nafame">input here:</label>
+            <input id='name' type='text'/>
+            <button style={{backgroundColor: 'red', color:'white'}}>
+                {/* {buttonText} */ getButtonText()}
+            </button>
+        </div>
+    );
+    //more information about translation to html can be found on "babeljs.io/repl"
 }
 
 // Take the react component and show it on the screen
